ecfa60285b0c589f60f5b6b162ecc04136335191
rodrigob1991 <rodrigito1991@hotmail.com>
2022-05-07 04:01:13 -0300

    ...

---
+++ pages/api/revalidate_home.ts
@@ -0,0 +1,30 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type {NextApiRequest, NextApiResponse} from 'next'
+
+type RevalidationResponseBody = {
+  message: string
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<RevalidationResponseBody>) {
+  let httpCode: number
+  let body: RevalidationResponseBody
+
+  if (req.query.secret !== process.env.REVALIDATION_HOME_TOKEN) {
+    httpCode = 401
+    body = {message: 'Invalid token'}
+  } else {
+
+    try {
+      await res.unstable_revalidate('/')
+      httpCode = 200
+      body = {message: 'successfully revalidated'}
+    } catch (err) {
+      // If there was an error, Next.js will continue
+      // to show the last successfully generated page
+      httpCode = 500
+      body = {message: 'Error revalidating'}
+    }
+  }
+
+  res.status(httpCode).json(body)
+}
