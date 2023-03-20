d870524a8968eb9f19e6120c80472c2297920c0a
rodrigob1991 <rodrigito1991@hotmail.com>
2023-03-20 01:57:10 -0300

    implementing websocket connection

--- apps/nextjs/src/components/home/header/messenger/chat/useWebSocket.ts
+++ apps/nextjs/src/components/home/header/messenger/chat/useWebSocket.ts
@@ -1,15 +1,34 @@
 import {UserType} from "chat-common/src/model/types"
 import {InboundToGuessMessage, InboundToHostMessage} from "./types"
+import {useEffect, useRef} from "react"
 
 type Props<UT extends UserType> = {
     userType: UT
     onMessage: (m: (UT extends "host" ? InboundToHostMessage : InboundToGuessMessage)) => void
 
 }
-export default function useWebSocket<UT extends UserType>({}: Props<UT>) {
-    const sendMessage = () => {
+export default function useWebSocket<UT extends UserType>({userType, onMessage}: Props<UT>) {
+    const wsEndpoint = `${process.env.WEBSOCKET_ENDPOINT}${userType === "host" ? "?host_user=" + process.env.PRIVATE_TOKEN : ""}`
+    const refToWs = useRef<WebSocket>()
+    const setWs = (ws: WebSocket) => {
+        refToWs.current = ws
+    }
+    const getWs = () => refToWs.current as WebSocket
+
+    useEffect(() => {
+            setWs(new WebSocket(wsEndpoint))
+            getWs().onmessage = (m) => {
+
+            }
+            return () => {
+                getWs().close()
+            }
+        }
+        , [])
+
+    const sendMessage = (body: string) => {
 
     }
 
-
+    return sendMessage
 }
