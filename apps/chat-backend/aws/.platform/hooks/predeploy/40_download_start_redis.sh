c01c5da7cc0efa54f03b886fd0945622ea368410
rodrigob1991 <rodrigito1991@hotmail.com>
2023-11-03 12:54:30 -0300

    redis conf

--- apps/chat-backend/aws/.platform/hooks/predeploy/40_download_start_redis.sh
+++ apps/chat-backend/aws/.platform/hooks/predeploy/40_download_start_redis.sh
@@ -3,5 +3,5 @@
 name="redis"
 
 if ! docker ps --format '{{.Names}}' | grep -w $name &> /dev/null; then
-    docker run -p 6379:6379 --name $name --restart unless-stopped -d $name
+    docker run -p 6379:6379 --name $name --restart unless-stopped -v chat-backend-data:/data -d $name redis-server --appendonly yes
 fi
