2bc0cd9552893e7c82fe2440c5897e8dda388b59
rodrigob1991 <rodrigito1991@hotmail.com>
2025-06-19 22:57:06 -0300

    meanly numbers types

--- packages/utils/src/transformer.ts
+++ packages/utils/src/transformer.ts
@@ -1,18 +1,20 @@
- import {TransformerFactory, SourceFile, Node, factory, isNumericLiteral, visitEachChild, visitNode, isSourceFile} from "typescript"
+ import {TransformerFactory, SourceFile, Node, factory, isNumericLiteral, visitEachChild, visitNode, isSourceFile, TypeChecker, Program, TransformationContext} from "typescript"
+ import {PluginConfig, TransformerExtras} from "ts-patch"
 
-const transformer: TransformerFactory<SourceFile> = (context) => {
-  console.table("Transformer initialized")
-  return (sourceFile) => {
-    const visitor = (node: Node): Node => {
-      if (isNumericLiteral(node)) {
-        // add "as Integer" after numeric literals
-        return node.parent
+const transformer = (program: Program, config: PluginConfig, extras: TransformerExtras) =>
+  (context: TransformationContext) => {
+    return (sourceFile: SourceFile) => {
+      const visitor = (node: Node): Node => {
+        if (isNumericLiteral(node)) {
+          // add "as Integer" after numeric literals
+          return factory.createAsExpression(node, factory.createTypeReferenceNode("Int"))
+        }
+
+        return visitEachChild(node, visitor, context)
       }
-      return visitEachChild(node, visitor, context)
-    }
 
-    return visitNode(sourceFile, visitor, isSourceFile)
-  }
+      return visitNode(sourceFile, visitor, isSourceFile)
+    }
 }
 
-export default transformer 
+export default transformer
--- packages/utils/src/types_checks.ts
+++ packages/utils/src/types_checks.ts
@@ -1,14 +1,28 @@
+export type Integer = number & { __brand: "integer" }
+export type NumberRange<F extends number, T extends number> = number & { __brand: "numberRange" }
+export type IntegerNumberRange<F extends Integer, T extends Integer> = Integer & { __brand: "numberRange" }
+
 export type KeyValue<K extends PropertyKey=PropertyKey, V=unknown>= Record<K, V>
 export type EmptyKeyValue = KeyValue<never, never>
 export type KeyNumberValue<K extends PropertyKey=PropertyKey, V extends number=number> = KeyValue<K, V>
+
 export type Callable<A extends unknown[]=[], R=unknown> = (...args: A) => R
+
 export type NonEmptyArray<T> = [T, ...T[]]
 
 export const isNumber = (v: unknown) : v is number => typeof v === "number"
+export const isInteger = (v: unknown) : v is Integer => typeof v === "number" && Number.isInteger(v)
+export const isNumberRange = <F extends number, T extends number>(v: unknown, f: F | undefined, t: T | undefined) : v is NumberRange<F, T> => typeof v === "number" && (f ? v >= f : true) && (t ? v <= t : true)
+export const isIntegerNumberRange = <F extends Integer, T extends Integer>(v: unknown, f: F | undefined, t: T | undefined) : v is IntegerNumberRange<F, T> => isNumberRange(v, f, t) && Number.isInteger(v)
+
 export const isString = (v: unknown) : v is string => typeof v === "string"
+
 export const isKeyValue = (v: unknown) : v is KeyValue => typeof v === "object" && !isArray(v) && v !== null
+export const isKeyNumberValue = (v: unknown) : v is KeyNumberValue => isKeyValue(v) && Object.values(v).every(v => isNumber(v))
+
 export const isCallable = (v: unknown) : v is Callable => typeof v === "function" && !v.toString().startsWith("class")
+
 export const isArray = (v: unknown) : v is Array<unknown> =>  Array.isArray(v)
 export const isNonEmpty = <T>(a: T[]): a is NonEmptyArray<T> => a.length > 0
 export const isNumberArray = (v: unknown) : v is Array<number> =>  Array.isArray(v) && v.every(v => isNumber(v))
-export const isKeyNumberValue = (v: unknown) : v is KeyNumberValue => isKeyValue(v) && Object.values(v).every(v => isNumber(v))
+
