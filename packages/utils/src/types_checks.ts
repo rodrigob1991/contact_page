3832366643c17664268518f34cb0582121236a89
rodrigob1991 <rodrigito1991@hotmail.com>
2025-06-20 12:03:02 -0300

    numbers types

--- packages/utils/src/types_checks.ts
+++ packages/utils/src/types_checks.ts
@@ -1,6 +1,10 @@
-export type Integer = number & { __brand: "integer" }
-export type NumberRange<F extends number, T extends number> = number & { __brand: "numberRange" }
-export type IntegerNumberRange<F extends Integer, T extends Integer> = Integer & { __brand: "numberRange" }
+export type IntegerNumber = number & { __brand: "integer_number" }
+export type NumberInRange<F extends number, T extends number> = number & { __brand: "number_in_range" }
+export type PositiveNumber = NumberInRange<0, number>
+export type NegativeNumber = NumberInRange<number, -1>
+export type IntegerNumberInRange<F extends IntegerNumber, T extends IntegerNumber> = NumberInRange<F, T> & IntegerNumber
+export type PositiveIntegerNumber = PositiveNumber & IntegerNumber
+export type NegativeIntegerNumber = NegativeNumber & IntegerNumber
 
 export type KeyValue<K extends PropertyKey=PropertyKey, V=unknown>= Record<K, V>
 export type EmptyKeyValue = KeyValue<never, never>
@@ -11,9 +15,13 @@ export type Callable<A extends unknown[]=[], R=unknown> = (...args: A) => R
 export type NonEmptyArray<T> = [T, ...T[]]
 
 export const isNumber = (v: unknown) : v is number => typeof v === "number"
-export const isInteger = (v: unknown) : v is Integer => typeof v === "number" && Number.isInteger(v)
-export const isNumberRange = <F extends number, T extends number>(v: unknown, f: F | undefined, t: T | undefined) : v is NumberRange<F, T> => typeof v === "number" && (f ? v >= f : true) && (t ? v <= t : true)
-export const isIntegerNumberRange = <F extends Integer, T extends Integer>(v: unknown, f: F | undefined, t: T | undefined) : v is IntegerNumberRange<F, T> => isNumberRange(v, f, t) && Number.isInteger(v)
+export const isIntegerNumber = (v: unknown) : v is IntegerNumber => isNumber(v) && Number.isInteger(v)
+export const isNumberInRange = <F extends number, T extends number>(v: unknown, f: F | undefined, t: T | undefined) : v is NumberInRange<F, T> => isNumber(v) && (f ? v >= f : true) && (t ? v <= t : true)
+export const isPositiveNumber = (v: unknown) : v is PositiveNumber => isNumberInRange(v, 0, undefined)
+export const isNegativeNumber = (v: unknown) : v is NegativeNumber => isNumberInRange(v, undefined, -1)
+export const isIntegerNumberInRange = <F extends IntegerNumber, T extends IntegerNumber>(v: unknown, f: F | undefined, t: T | undefined) : v is IntegerNumberInRange<F, T> => isNumberInRange(v, f, t) && Number.isInteger(v)
+export const isPositiveIntegerNumber = (v: unknown) : v is PositiveIntegerNumber => isIntegerNumberInRange(v, 0 as IntegerNumber, undefined)
+export const isNegativeIntegerNumber = (v: unknown) : v is NegativeIntegerNumber => isIntegerNumberInRange(v, undefined, -1 as IntegerNumber)
 
 export const isString = (v: unknown) : v is string => typeof v === "string"
 
