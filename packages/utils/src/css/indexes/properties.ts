2fae7541b1696077c9db124ec77bf1db2fa38254
rodrigob1991 <rodrigito1991@hotmail.com>
2025-07-30 23:22:43 -0300

    types

--- packages/utils/src/css/indexes/properties.ts
+++ packages/utils/src/css/indexes/properties.ts
@@ -1,5 +1,7 @@
+import { CamelOrPascalToKebab } from "src/types"
 import { toCase } from "../../strings"
-import { CSSKeywords, LengthPercentage, LineWidth, ValueProducer, valueProducer } from "./values"
+import { CSSKeywords, LengthPercentage, LineWidth, ValueProducer, ValueProducerResult, valueProducer } from "./values"
+import { KeyValue } from "src/types_checks"
 
 export const cssPropertiesProducer = {
     borderWidth: valueProducer as ValueProducer<[a: LineWidth, b?:LineWidth, c?: LineWidth, d?:LineWidth], CSSKeywords["none"]>,
@@ -8,12 +10,11 @@ export const cssPropertiesProducer = {
     width: valueProducer as ValueProducer<[a: LengthPercentage]>
 } as const
 
-type KeyArgs<K extends CSSPropertyKey> = K extends CSSPropertyKey ? [K, CSSPropertyArgs<K>] : never
+type KeyArgs<K extends CSSPropertyKey=CSSPropertyKey> = K extends CSSPropertyKey ? [K, CSSPropertyArgs<K>] : never
 type KeysArgs<KL extends CSSPropertyKey[]=CSSPropertyKey[]> = KL extends [infer K extends CSSPropertyKey, ...infer R extends CSSPropertyKey[]] ? [[K, CSSPropertyArgs<K>], ...KeysArgs<R>] : KeyArgs<KL[number]>[]
 
-export type CSSPropertiesStr<K extends CSSPropertyKey=CSSPropertyKey> = {
-    [OK in K]: ReturnType<CSSPropertiesProducer[OK]>
-}
+export type CSSPropertyStr<KA extends KeyArgs> = KA extends KeyArgs ? `${CamelOrPascalToKebab<KA[0]>}: ${ValueProducerResult<KA[1]>};` : never
+export type CSSPropertiesStr<KA extends KeysArgs> = KA extends [infer F extends KeyArgs, ...infer R extends KeysArgs] ? `${CSSPropertyStr<F>}${CSSPropertiesStr<R>}` : KA extends [] ? "" : string
 export const getCssPropertiesStr = <KA extends KeysArgs>(...keysArgs: KA) => {
     let str = ""
     for (const [key, args] of keysArgs) {
@@ -23,11 +24,9 @@ export const getCssPropertiesStr = <KA extends KeysArgs>(...keysArgs: KA) => {
     return str as CSSPropertiesStr<KA>
 }
 
-export type CSSPropertiesKeyValue<K extends CSSPropertyKey=CSSPropertyKey> = {
-    [OK in K]: ReturnType<CSSPropertiesProducer[OK]>
-}
+export type CSSPropertiesKeyValue<KA extends KeysArgs> = KA extends [infer F extends KeyArgs, ...infer R extends KeysArgs] ? {[K in F[0]]: ValueProducerResult<F[1]>} & CSSPropertiesKeyValue<R> : KA extends [] ? {} : KeyValue<KA[number][0], ValueProducerResult<KA[number][1]>>
 export const getCssPropertiesKeyValue = <KA extends KeysArgs>(...keysArgs: KA) => {
-    const keyValue: Partial<CSSPropertiesKeyValue> = {}
+    const keyValue: Partial<CSSPropertiesKeyValue<KA>> = {}
     for (const [key, args] of keysArgs) {
         const value = cssPropertiesProducer[key](...args)
         keyValue[key] = value
